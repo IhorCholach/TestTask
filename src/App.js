8d28207114f013e33482e16691a9aa7a1deaefa2
Ihor Cholach <ihorcholach@gmail.com>
2020-08-14 15:53:46 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let rrr = 10;
+  const array1 = ['a', 'b', 'c'];
+
+  const ttt = () => {
+    // while (rrr) {
+    //   console.log('rrr', rrr);
+    //   setTimeout(() => console.log(new Date()));
+    //   rrr--;
+    // }
+    // for (let i = 0; i < 10; i++) {
+    //   console.log('cikle', i);
+    //   setTimeout(() => {
+    //     console.log(i);
+    //   });
+    // }
+    // for (const element of array1) {
+    //   console.log('FOR_IN', element);
+    //   setTimeout(() => {
+    //     console.log(element);
+    //   });
+    // }
+    // for (const element in array1) {
+    //   console.log('FOR_OF', element);
+    //   setTimeout(() => {
+    //     console.log(element);
+    //   });
+    // }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <button onClick={ttt}>CLICK</button>
         </p>
         <a
           className="App-link"
